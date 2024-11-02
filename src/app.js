fe1e78189a2ece1a1f47c5ccfe09a4e1e8f276bc
Shahid Nabi <124462771+PrinceShahidYT@users.noreply.github.com>
2024-11-02 09:07:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const getUrl = require('./getUrl')
-
+const getUrl = require('./getUrl');
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 7860
