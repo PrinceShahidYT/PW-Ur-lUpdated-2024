7dbffc5d7bf9694401221e7e84804c665d5aa19b
Shahid Nabi <124462771+PrinceShahidYT@users.noreply.github.com>
2024-11-02 09:04:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,9 +9,16 @@ const PORT = process.env.PORT || 7860
 
 app.use('/', getUrl);
 
-app.listen(PORT, () => {
-    console.log(`Working on Port: ${PORT}`);
-    
-})
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server Connected on Port: ${PORT}`);
+        });
+    } catch (error) {
+        console.log(`Can't connect to the server: ${error.message}`);
+    }
+}).catch((error) => {
+    console.log("Mongoose Connection Error: " + error.message);
+});
 
 module.exports = app;
